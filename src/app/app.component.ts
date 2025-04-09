726e27cb66371c15c6c932de286e9b897f6655d5
ssCuacKss <proldanpuebla@gmail.com>
2025-04-09 12:39:43 +0200

    ha habido que reiniciar el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, viewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Gantt from 'frappe-gantt'
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+   <h1>Welcome to {{title}}!</h1>
+    <div #ganttContainer ></div>
     <router-outlet />
   `,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'MoreSchedule';
+  @ViewChild('ganttContainer', {static: true}) ganttContainer!:ElementRef;
+
+  ngAfterViewInit(): void {
+    const tasks = [
+      {
+        id: 'Task 1',
+        name: 'Tarea 1',
+        start: '2025-04-01',
+        end: '2025-04-05',
+        progress: 20,
+      },
+      {
+        id: 'Task 2',
+        name: 'Tarea 2',
+        start: '2025-04-03',
+        end: '2025-04-10',
+        progress: 40,
+        dependencies: 'Task 1'
+      }
+    ];
+
+    new Gantt(this.ganttContainer.nativeElement, tasks, {container_height: 700});
+
+  }
 }
