10987fdb97fc5916403ad0dc759dc740378ad479
ssCuacKss <proldanpuebla@gmail.com>
2025-04-11 13:50:17 +0200

    iniciadas pruebas satisfactorias con ngx-gantt

--- src/app/schedule-chart/schedule-chart.component.ts
+++ src/app/schedule-chart/schedule-chart.component.ts
@@ -1,12 +1,12 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { GanttItem} from '@worktile/gantt';
+import { GanttItem, GanttViewType} from '@worktile/gantt';
 @Component({
   selector: 'app-schedule-chart',
   standalone: false,
   template: `
     <p>
       schedule-chart works!
-      <ngx-gantt #gantt [items]="items">
+      <ngx-gantt #gantt [items]="items" [viewType]='view' [start]="start" [end]="end">
         <ngx-gantt-table>
           <ngx-gantt-column name="Title" width="300px">
             <ng-template #cell let-item="item"> {{ item.title }} </ng-template>
@@ -19,18 +19,27 @@ import { GanttItem} from '@worktile/gantt';
 })
 export class ScheduleChartComponent{
 
-  items: GanttItem[] = [{
-    id: '000000',
-    title: 'Task 0',
-    start: new Date('2021-07-31T08:00:00'),
-    end: new Date('2021-08-08T17:00:00')
-  },
-  {
-    id: '000001',
-    title: 'Task 1',
-    start: new Date('2021-04-02T08:00:00'),
-    end: new Date('2021-07-05T17:00:00')
-  }];
+  view = GanttViewType.hour;
+  start = new Date('2025-07-26').getTime();
+  end = new Date('2025-08-03').getTime();
+
+  items: GanttItem[] = [
+    {
+      id: '000000',
+      title: 'Task 0',
+      start: new Date('2025-07-28T08:00:00'),
+      end: new Date('2025-07-29T17:00:00'),
+      expandable: true,
+    },
+    {
+      id: '000001',
+      title: 'Task 1',
+      start: new Date('2025-08-02T08:00:00'),
+      end: new Date('2025-08-02T17:00:00'),
+      expandable: true,
+      links: ['000000']
+    }
+  ];
 
 
   constructor() {}
