423f9848d16d10f01d8ae0fda80a79799f2b9aa7
ssCuacKss <proldanpuebla@gmail.com>
2025-05-14 13:31:43 +0200

    añadida barra de menu para el calendario

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideNoopAnimations} from '@angular/platform-browser/animations'
 
 import routeConfig from './routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routeConfig)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routeConfig), provideNoopAnimations()]
 };
--- src/app/schedule-calendar/schedule-calendar.component.ts
+++ src/app/schedule-calendar/schedule-calendar.component.ts
@@ -1,29 +1,59 @@
-import { Component, LOCALE_ID } from '@angular/core';
-import { CalendarDateFormatter, CalendarEvent, CalendarModule, DateAdapter } from 'angular-calendar';
+import { Component, ElementRef, inject, LOCALE_ID } from '@angular/core';
+import { CalendarDateFormatter, CalendarEvent, CalendarModule, CalendarMonthViewDay, DateAdapter } from 'angular-calendar';
 import { SchedulerDateFormatter, SchedulerModule } from 'angular-calendar-scheduler';
-import { startOfDay, addHours, addMonths, subMonths } from 'date-fns';
+import { startOfDay, addHours, addMonths, subMonths, isSameMonth, isSameDay, sub } from 'date-fns';
 import { Router } from '@angular/router';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-schedule-calendar',
   imports: [CommonModule ,CalendarModule, SchedulerModule],
   template: `
     
+    <div class="menu">
+      <ul class="calendarMenu">
+        <li (click)="openMenu($event)" class="item" id="menu-one">Proyectos
+          <ul class="closed" (mouseleave)="closeMenu($event)">
+            <li>Nuevo Proyecto</li>
+            <li>Nuevo Proyecto sin Plantilla</li>
+            <li>Editar Proyecto</li>
+          </ul>
+        </li>
+        <li (click)="openMenu($event)" class="item" id="menu-four">Plantillas
+          <ul class="closed" (mouseleave)="closeMenu($event)">
+            <li>Nueva Plantilla</li>
+            <li>Editar Plantilla</li>
+            <li>Eliminar Plantilla</li>
+          </ul>
+        </li>
+        <li (click)="openMenu($event)" class="item" id="menu-two">Usuarios
+          <ul class="closed" (mouseleave)="closeMenu($event)">
+            <li>Crear Usuario</li>
+            <li>Editar Usuario</li>
+            <li>Eliminar Usuario</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="calendar-controls">
+      <button id="prevMonth" class="calendarButton" (click)="previousMonth()">Mes<br>anterior</button>
+      <p style="width: 7rem; text-align: center;">
+        {{viewDate.toLocaleDateString('default', {month: 'long'})}}<br>{{viewDate.getFullYear()}}
+      </p>
+      <button id="nextMonth" class="calendarButton" (click)="nextMonth()">Mes<br>siguiente</button>
+    </div>
     <div style="padding: 1rem;" class="calendar">
+    
       <mwl-calendar-month-view
         [viewDate]="viewDate"
         [events]="events"
-        (eventClicked)="goToProyectSchedule($event.event)"/>
+        (eventClicked)="goToProyectSchedule($event.event)"
+        (dayClicked)="ampliarDia($event.day)"
+        [activeDayIsOpen]="activeDayIsOpen"/>
     </div>
-    <div class="calendar-controls">
-      <button (click)="previousMonth()">&lt; Mes Anterior</button>
-      <p style="width: 7rem; text-align: center;">
-        {{viewDate.toLocaleDateString('default', {month: 'long'})}} {{viewDate.getFullYear()}}
-      </p>
-      <button (click)="nextMonth()">Mes Siguiente &gt;</button>
-    </div>
+    
     
   `,
   styleUrl: './schedule-calendar.component.css',
@@ -41,6 +71,9 @@ import localeEs from '@angular/common/locales/es';
 export class ScheduleCalendarComponent  {
 
   viewDate: Date = new Date();
+  activeDayIsOpen: boolean = false;
+  document:Document = inject(DOCUMENT);
+  router: Router = inject(Router);
   events: CalendarEvent[] = [
     {
       start: startOfDay(new Date("05-06-2025")),
@@ -62,8 +95,17 @@ export class ScheduleCalendarComponent  {
     }
   ];
 
-  constructor(private router: Router){
+  constructor(){
     registerLocaleData(localeEs);
+    /*this.document.body.addEventListener("click", (event: Event) =>{
+      let menuItems = this.document.querySelectorAll('.item');
+      menuItems.forEach(item => {
+        let submenu = item.querySelector("ul");
+        if(submenu){
+          submenu.className = "closed";
+        }
+      });
+    })*/
   }
 
   previousMonth(): void {
@@ -74,6 +116,18 @@ export class ScheduleCalendarComponent  {
     this.viewDate = addMonths(this.viewDate, 1);
   }
 
+  public ampliarDia(dia: CalendarMonthViewDay){
+    
+    if(isSameMonth(dia.date, this.viewDate)){
+      
+      if((isSameDay(dia.date, this.viewDate) && this.activeDayIsOpen )|| dia.events.length === 0){
+        this.activeDayIsOpen = false;
+      }else{
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = dia.date;
+    }
+  }
 
   public goToProyectSchedule(event: CalendarEvent): void {
 
@@ -82,5 +136,35 @@ export class ScheduleCalendarComponent  {
 
   }
 
+  public openMenu(event: Event){
+    const element = (event.currentTarget as HTMLLIElement)
+    const selectable = element.querySelector('ul');
+    let menuItems = this.document.querySelectorAll('.item');
+   
+    menuItems.forEach(item => {
+      if(item.id !== element.id){
+        let submenu = item.querySelector("ul");
+        if(submenu){
+          submenu.className = "closed";
+        }
+      }
+    });
+    if(selectable){
+      if(selectable.className === "closed"){
+        selectable.className = "dropdown";
+      }else{
+        selectable.className = "closed";
+      }
+    }
+  }
+
+  public closeMenu(event: Event){
+    const subMenu = event.target as HTMLUListElement;
+    if(subMenu.className !== "closed"){
+      subMenu.className = "closed";
+    }
+  }
+
+  
 
 }
